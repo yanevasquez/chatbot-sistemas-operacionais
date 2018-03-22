5be0dcfe2848f3f6bb253f7cd4f0976e7a740a17
Yane <30804152+yanevasquez@users.noreply.github.com>
2018-03-21 23:21:24 -0300

    script básico conversação

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,1 +1,11 @@
-*script do bot*
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+bot = ChatBot('Test')
+
+conv = ['Oi', 'olá']
+bot.set_trainer(ListTrainer)
+bot.train(conv)
+while(True):
+    quest = input('Você: ')
+    response = bot.get_response(quest)
+    print('Bot: ', response)
